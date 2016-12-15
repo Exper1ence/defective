b2f095fc335588b9ed5d378aa2fb076aa5f04eb8
Exper1ence <3533357503@qq.com>
2016-12-15 12:39:10 +0800

    add globals stub

---
+++ defective-globals-stub.js
@@ -0,0 +1,36 @@
+/**
+ * Created by Exper1ence on 2016/12/15.
+ */
+/**
+ * Loads a module.
+ * See <a href="http://nodejs.org/api/modules.html">http://nodejs.org/api/modules.html</a> for more information.
+ * @param {String} target
+ * @param {Function} suit
+ * @returns {*}
+ */
+function describe(target, suit) {
+}
+/**
+ * Loads a module.
+ * See <a href="http://nodejs.org/api/modules.html">http://nodejs.org/api/modules.html</a> for more information.
+ * @param {String} behavior
+ * @param {Function} spec
+ * @returns {*}
+ */
+function it(behavior, spec) {
+}
+/**
+ * Loads a module.
+ * See <a href="http://nodejs.org/api/modules.html">http://nodejs.org/api/modules.html</a> for more information.
+ * @param {*} expression
+ * @returns {Object}
+ */
+function expect(expression) {
+}
+/**
+ * Loads a module.
+ * See <a href="http://nodejs.org/api/modules.html">http://nodejs.org/api/modules.html</a> for more information.
+ * @param {*} expectation
+ */
+function toBe(expectation) {
+}
--- defective.js
+++ defective.js
@@ -1,3 +1,26 @@
 /**
  * Created by Exper1ence on 2016/12/14.
  */
+require('./defective-globals-stub');
+const _ = require('lodash');
+const vm = require('vm');
+const fs = require('fs');
+
+module.exports = function (cb) {
+    if (!_.isFunction(cb))throw new TypeError('The callback should be a Function.');
+    vm.runInNewContext(`(${cb.toString()})()`, {
+        describe(target, suit){
+            suit();
+        },
+        it(behavior, spec){
+            spec();
+        },
+        expect(expression){
+            return {
+                toBe(expectation){
+                    console.log(expression == expectation);
+                }
+            }
+        }
+    });
+};
